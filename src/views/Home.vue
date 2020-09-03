48886fe54dc7beb7b4b742e7b56dce6c733c301d
Locky Keaney <lockykeaney@protonmail.com>
2020-09-03 18:11:47 +1000

    initial

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <HelloWorld name="Locky Keaney" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { HelloWorld } from "../components";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+});
+</script>
+
+<style lang="scss">
+.home {
+  height: 100vh;
+}
+</style>
